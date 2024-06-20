f1371b884148f1d0a87de4b5f5dad7749de89f65
Léa <lea@gmail.com>
2024-06-20 14:28:11 +0200

    fv

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 print('momo la grint')
+h
+returnrtb
+tr
+btr
+gv
+trb
+grn
+hjtygbrvf
+gbhnj,uynhgrtefd
+vgthnyjuht
+grfhyjuihtbgrvf
+hyjukijygthfvdbhkiuj,yhngtrfbhynkiu
+,jyhgbfnjy,kiu,jyhngfbn
+jy,unhgbfngyju
